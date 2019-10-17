64883c3c086fbdc9fa0f703e7d012320448dcf12
ganganyyy <ganganyyy0612@gmail.com>
2019-10-17 13:02:41 +0800

    init
    init

---
+++ src/main/java/com/ssm/tpssystem/TpssystemApplication.java
@@ -0,0 +1,13 @@
+package com.ssm.tpssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssm/tpssystem/TpssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssm.tpssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
