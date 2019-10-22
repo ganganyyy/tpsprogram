30f27b42a9ed1e70256e84d0fc871016e1dc78d0
Icegrain <wangbingchaofun@163.com>
2019-10-22 13:12:10 +0800

    update
    update

---
+++ src/main/java/com/ssm/tpssystem/utils/BackOfiice.java
@@ -0,0 +1,14 @@
+package com.ssm.tpssystem.utils;
+
+import com.ssm.tpssystem.domain.Interaction;
+import com.ssm.tpssystem.domain.Trade;
+import com.ssm.tpssystem.domain.Transaction;
+
+public class BackOfiice {
+    private Interaction interaction;
+    private Trade trade;
+    private Transaction transaction;
+
+    public
+
+}
